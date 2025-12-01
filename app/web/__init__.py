579bb1efdce9f3cce3e8df5cda06a3f167bf7931
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-30 23:41:14 -0600

    Update __init__.py

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,6 +1,10 @@
-#app/web/__init__.py
+# app/web/__init__.py
 from flask import Blueprint
 
-bp = Blueprint("web", __name__)
+bp = Blueprint(
+    "web",
+    __name__,
+    template_folder="templates"
+)
 
 from . import routes
