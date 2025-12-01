1e0654042bd98e229e5abb125a9428c5465bfecf
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-30 22:32:30 -0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from .db import engine, Base
+from .webapp import bp as web_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "dev-only-change-me"
+
+    # Ensure tables exist
+    Base.metadata.create_all(bind=engine)
+
+    # Register web UI
+    app.register_blueprint(web_bp)
+
+    return app
