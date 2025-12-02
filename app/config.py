95c0d0562c85fda2380e31c1aca8b3a4d6495467
twbaty <51901395+twbaty@users.noreply.github.com>
2025-12-01 22:54:31 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 # app/config.py
 import os
 from dotenv import load_dotenv
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
+DB_PATH = f"sqlite:///{os.path.join(BASE_DIR, '..', 'cvescout.db')}"
 
 load_dotenv()
 
