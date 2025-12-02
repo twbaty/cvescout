341f9ccac829fbe48dfbb93017428d1a97e6abfc
twbaty <51901395+twbaty@users.noreply.github.com>
2025-12-01 22:44:36 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,13 +1,10 @@
+# app/config.py
 import os
-from pathlib import Path
+from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parents[1]
+load_dotenv()
 
-DB_PATH = os.environ.get(
-    "CVESCOUT_DB",
-    str(BASE_DIR / "cve_scout.db")
-)
+DB_PATH = os.environ.get("DB_PATH", "sqlite:///cvescout.db")
 
-# NVD_API_KEY = os.environ.get("NVD_API_KEY", "")
-NVD_API_KEY = c26b6c93-5fe7-4458-aca7-fe3dd61b7ce0
-NVD_BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+# NVD API key comes ONLY from environment
+NVD_API_KEY = os.environ.get("NVD_API_KEY", "")
