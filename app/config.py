83ecfacfdf5b05144a97186fcec9c7ca2f1c9cf9
twbaty <51901395+twbaty@users.noreply.github.com>
2025-12-01 22:33:47 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,5 +8,6 @@ DB_PATH = os.environ.get(
     str(BASE_DIR / "cve_scout.db")
 )
 
-NVD_API_KEY = os.environ.get("NVD_API_KEY", "")
+# NVD_API_KEY = os.environ.get("NVD_API_KEY", "")
+NVD_API_KEY = c26b6c93-5fe7-4458-aca7-fe3dd61b7ce0
 NVD_BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
